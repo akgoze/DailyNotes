ccdef5d12c3a1e2de5226e71fbfe3141747354b1
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2023-04-08 14:40:26 +0300

    Added editorjs configuration.

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-import WelcomeItem from "./WelcomeItem.vue";
-import DocumentationIcon from "./icons/IconDocumentation.vue";
-import ToolingIcon from "./icons/IconTooling.vue";
-import EcosystemIcon from "./icons/IconEcosystem.vue";
-import CommunityIcon from "./icons/IconCommunity.vue";
-import SupportIcon from "./icons/IconSupport.vue";
-
 import { useCollection, useFirestore } from "vuefire";
 import { collection } from "firebase/firestore";
+
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
+
+const editor = new EditorJS({
+  holder: "editorjs",
+
+  tools: {
+    header: Header,
+    list: List,
+  },
+});
 
 const db = useFirestore();
 
@@ -15,5 +21,6 @@ const notes = useCollection(collection(db, "notes"));
 </script>
 
 <template>
-  <pre>{{ notes }}</pre>
+  <div id="editorjs"></div>
+  <!-- <pre>{{ notes }}</pre> -->
 </template>
