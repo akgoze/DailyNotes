fba170d8370f0b2ea9dad2f46b6c13813e785463
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2023-05-01 20:44:51 +0300

    Removed functions

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import { firebaseApp } from "./firebase";
-import { VueFire, VueFireAuth } from "vuefire";
+// import { firebaseApp } from "./firebase.ts";
+// import { VueFire, VueFireAuth } from "vuefire";
 
 createApp(App)
-  .use(VueFire, {
-    // imported above but could also just be created here
-    firebaseApp,
-    modules: [
-      // we will see other modules later on
-      VueFireAuth(),
-    ],
-  })
+  // .use(VueFire, {
+  //   // imported above but could also just be created here
+  //   // firebaseApp,
+  //   modules: [
+  //     // we will see other modules later on
+  //     // VueFireAuth(),
+  //   ],
+  // })
   .mount("#app");
 
 import "./assets/main.scss";
