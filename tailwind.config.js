6825acf86b63b705ac6ca2808476592fc548aaa6
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2023-10-07 14:08:33 +0300

    style(font): Added main font family.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Averia Serif Libre", "cursive"],
+    },
+    extend: {
+      fontFamily: {
+        sans: [
+          "Averia Serif Libre",
+          "cursive",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+    },
   },
   plugins: [],
 };
