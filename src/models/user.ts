a51cc83bf87b34aad1a776e6da74395eaa257219
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2023-10-15 22:04:50 +0300

    feat(Model): Added user model.

---
+++ src/models/user.ts
@@ -0,0 +1,11 @@
+type User = {
+  uid?: string;
+  email?: string;
+  first_name?: string;
+  last_name?: string;
+  photo_url?: string;
+  created_at?: Date;
+  last_login_at?: Date;
+};
+
+export type { User };
